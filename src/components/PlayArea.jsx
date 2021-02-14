6d31c32533495479f990079fb82c7629e4daa954
Rodriques <rodriques.perry@gmail.com>
2021-02-14 13:04:18 -0600

    Header Component

---
+++ src/components/PlayArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlayArea = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlayArea
---
+++ src/shared/dice.js
@@ -0,0 +1,26 @@
+export const DICE = [
+  {
+    id: 1,
+    image: "../../images/dice1.png"
+  },
+  {
+    id: 2,
+    image: "../../public/images/dice2.png"
+  },
+  {
+    id: 3,
+    image: "../../public/images/dice3.png"
+  },
+  {
+    id: 4,
+    image: "../../public/images/dice4.png"
+  },
+  {
+    id: 5,
+    image: "../../public/images/dice5.png"
+  },
+  {
+    id: 6,
+    image: "../../public/images/dice6.png"
+  }
+];
