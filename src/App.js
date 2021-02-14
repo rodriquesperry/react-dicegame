db98c0fed9af7820b07510222f1817e27ef92a5c
Rodriques <rodriques.perry@gmail.com>
2021-02-14 13:11:07 -0600

    PlayArea Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import PlayArea from './components/PlayArea'
 import './App.css'
 
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div>
       <Header />
+      <PlayArea />
     </div>
   )
 }
--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const PlayArea = () => {
   return (
     <div>
-      
+      <div className='players'>
+        <div className='player'>
+          <h2>Player 1</h2>
+          <img src='../../images/dice1.png' />
+        </div>
+        <div className='player'>
+          <h2>Player 2</h2>
+          <img src='../../images/dice2.png' />
+        </div>
+      </div>
     </div>
   )
 }
